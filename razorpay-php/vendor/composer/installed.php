5c51c73a60b200a1a8efd338c462167fa60aca00
Junaid mugloo <33572920+junaidmugloo@users.noreply.github.com>
2024-07-09 12:41:39 +0530

    foxx2

---
+++ razorpay-php/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'razorpay/razorpay',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '280d650dc459de6ac798161a39cc94f2c4a9c816',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'raveren/kint' => array(
+            'pretty_version' => 'v1.2.10',
+            'version' => '1.2.10.0',
+            'reference' => '16b6ed64d7c3dfd215f44497f98fb1d49cdbdd43',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../raveren/kint',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'razorpay/razorpay' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '280d650dc459de6ac798161a39cc94f2c4a9c816',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rmccue/requests' => array(
+            'pretty_version' => 'v1.6.1',
+            'version' => '1.6.1.0',
+            'reference' => '6aac485666c2955077d77b796bbdd25f0013a4ea',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rmccue/requests',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
